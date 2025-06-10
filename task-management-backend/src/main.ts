4eaed16500885a2fb52b698de18c2e2789c4563f
Usama Saleem <lozifa226@gmail.com>
2025-06-10 22:29:43 +0500

    Update main.ts

--- task-management-backend/src/main.ts
+++ task-management-backend/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express'; // Import Express platform
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
