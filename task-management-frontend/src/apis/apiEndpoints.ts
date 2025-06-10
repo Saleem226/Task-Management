60a16de686db2368cdaff3b74f0e04ef2be5569f
Usama Saleem <lozifa226@gmail.com>
2025-06-11 01:26:48 +0500

    FE Changes

--- task-management-frontend/src/apis/apiEndpoints.ts
+++ task-management-frontend/src/apis/apiEndpoints.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL: string =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001";
 
 export const API_ENDPOINTS = {
   socket: `${API_BASE_URL}`,
--- task-management-frontend/src/app/dashboard/page.tsx
+++ task-management-frontend/src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify"; // new import
